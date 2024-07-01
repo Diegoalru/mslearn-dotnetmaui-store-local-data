da135ddf3109660e868ee06c850c45db3cb6cfc1
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-07-01 15:02:31 -0600

    Enhance AddNewPerson method functionality.

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -26,25 +26,22 @@ public class PersonRepository(string dbPath)
 
     public void AddNewPerson(string name)
     {
-        int result = 0;
         try
         {
-            // TODO: Call Init()
+            Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
-            result = 0;
+            var result = _conn.Insert(new Person { Name = name });
 
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            StatusMessage = $"{result} record(s) added (Name: {name})";
         }
         catch (Exception ex)
         {
-            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            StatusMessage = $"Failed to add {name}. Error: {ex.Message}";
         }
-
     }
 
     public List<Person> GetAllPeople()
