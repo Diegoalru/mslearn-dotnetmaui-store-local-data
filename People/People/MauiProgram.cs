cf60a61f2c2716617def68a1ad059d52a2ce14dd
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-07-01 15:18:08 -0600

    Create route of the dbPath and Singleton.

--- People/People/MauiProgram.cs
+++ People/People/MauiProgram.cs
@@ -2,19 +2,21 @@
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		// TODO: Add statements for adding PersonRepository as a singleton
-			
+        var dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+        builder.Services.AddSingleton<PersonRepository>(options => 
+            ActivatorUtilities.CreateInstance<PersonRepository>(options, dbPath));
+
         return builder.Build();
-	}
+    }
 }
